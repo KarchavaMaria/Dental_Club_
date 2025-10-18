184e6ecfab9c21d2dd7fc194400d38fb0af7559f
KovalyovaMaria <idKovalyova@gmail.com>
2025-10-18 14:33:28 +0300

    HashRouter

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,17 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import ScrollToTop from "./ScrollToTop";
 import {ToastContainer} from "react-toastify";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
+        <HashRouter>>
             <ScrollToTop/>
             <App/>
             <ToastContainer />
-        </BrowserRouter>
+        </HashRouter>>
     </React.StrictMode>
 );
